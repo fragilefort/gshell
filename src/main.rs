aacb223c8fe8fb689a9c6a1e94bf323ee0c5019b
fragilefort <imaddex3759@gmail.com>
2026-09-19 23:35:43 +0200

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,16 @@
 use std::io::{self, Write};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    print!("$ ");
-    io::stdout().flush().unwrap();
+    loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
 
-    let mut buffer = String::new();
-    let stdin = io::stdin();
-    stdin.read_line(&mut buffer)?;
-    buffer = buffer.trim().to_string();
-    print!("{}: command not found", buffer);
-    Ok(())
+        let mut buffer = String::new();
+        let stdin = io::stdin();
+        stdin.read_line(&mut buffer)?;
+        let buffer = buffer.trim().to_string();
+        print!("{}: command not found", buffer);
+
+        return Ok(());
+    }
 }
