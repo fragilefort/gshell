23160c355ea9a3cc291e3139cc8e6f3ea1d87c02
fragilefort <imaddex3759@gmail.com>
2026-09-20 19:56:41 +0200

    Adding support for echo
    instead if using if statements for each command, store all the commands
    in a hashmap and call them using user input. OW, print command not found

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,16 @@
+use std::collections::HashMap;
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::sync::LazyLock;
+
+type CommandFn = fn(Option<&str>) -> Result<(), Box<dyn std::error::Error>>;
+
+static COMMANDS: LazyLock<HashMap<&'static str, CommandFn>> = LazyLock::new(|| {
+    HashMap::from([
+        ("exit", shell_exit as CommandFn),
+        ("echo", echo as CommandFn),
+    ])
+});
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     loop {
@@ -10,9 +21,27 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         let stdin = io::stdin();
         stdin.read_line(&mut buffer)?;
         let buffer = buffer.trim();
-        if buffer == "exit" {
-            return Ok(());
+        match COMMANDS.get(buffer) {
+            Some(command) => call(*command, None)?,
+            None => println!("{}: command not found", buffer),
         }
-        println!("{}: command not found", buffer);
+    }
+}
+
+fn call(f: CommandFn, args: Option<&str>) -> Result<(), Box<dyn std::error::Error>> {
+    f(args)
+}
+
+fn shell_exit(_args: Option<&str>) -> Result<(), Box<dyn std::error::Error>> {
+    std::process::exit(0);
+}
+
+fn echo(_args: Option<&str>) -> Result<(), Box<dyn std::error::Error>> {
+    match _args {
+        None => Ok(()),
+        Some(text) => {
+            println!("{}", text);
+            Ok(())
+        }
     }
 }
