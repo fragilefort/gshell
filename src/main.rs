20529307fe326cf406f1d3e452587cde894591af
fragilefort <imaddex3759@gmail.com>
2026-09-21 15:19:12 +0200

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -75,7 +75,11 @@ fn type_(_args: Option<&str>) -> Result<(), Err> {
 }
 
 fn find_exec(command: &str) -> Option<PathBuf> {
-    env::split_paths(&env::var_os("PATH")?)
+    let path = env::var_os("PATH")?;
+
+    println!("DEBUG PATH: {:?}", path);
+
+    env::split_paths(&path)
         .map(|path| path.join(command))
         .find(|path| is_executable(path))
 }
